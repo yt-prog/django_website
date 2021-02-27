464ec080b4d9bfe5cfe9d869f15eb129728bdec6
Yutaka Tanaka <yt.subconscious+github@gmail.com>
2021-02-27 17:39:19 +0900

    set migrate

--- django_website/settings.py
+++ django_website/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'or_##f=%3xl@-q23yug2*hva@ft7%p^h2r%#r7f+)f*7+d)%*6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
