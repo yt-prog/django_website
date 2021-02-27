05af72830927ce0e9506ae5a4c40511a4f076c81
Yutaka Tanaka <yt.subconscious+github@gmail.com>
2021-02-27 17:19:57 +0900

    after postgres installed

--- django_website/settings.py
+++ django_website/settings.py
@@ -76,11 +76,14 @@ WSGI_APPLICATION = 'django_website.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+import dj_database_url
+from dotenv import (
+    find_dotenv,
+    load_dotenv,
+)
+load_dotenv(find_dotenv())
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(conn_max_age=600),
 }
 
 
